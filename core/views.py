844ad8ddccaa40ddc87afa6a5883204f6dc7d6f9
shimul <shimul929@gmail.com>
2022-03-14 00:55:18 +0600

    webrtc test

--- core/views.py
+++ core/views.py
@@ -1,5 +1,12 @@
+from multiprocessing import context
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def webrtc(request):
+    context = {
+        'usr': request.GET.get('user', 'NA') 
+    }
+    return render(request, 'webrtc.html', context)
